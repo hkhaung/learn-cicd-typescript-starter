9dd70a2fec2806de2eda170ee564149b6c9c63e1
hkhaung <henrykhaung22@gmail.com>
2025-12-29 23:36:39 -0600

    add tests

---
+++ src/tests/get-api-key-test.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from "vitest";
+import {getAPIKey} from '../api/auth.ts';
+
+describe("getAPIKey", () => {
+  test("test get api key no auth header", () => {
+    expect(getAPIKey({}), null);
+  })
+})
